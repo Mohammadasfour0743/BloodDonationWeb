a31abf2ccb7b49bce782f1b69148c0808c35013c
Peter Herczku <peterherczku915@gmail.com>
2025-04-16 08:27:51 +0200

    started working on donation history page

--- src/view/DonationHistoryTableView.js
+++ src/view/DonationHistoryTableView.js
@@ -1,16 +1,18 @@
-const rows = [0, 1, 2, 3, 4, 5, 6];
-
-export function DonationHistoryTableView() {
+export function DonationHistoryTableView({ requests }) {
   return (
     <div>
-      <h1>Donation History</h1>
-      {rows
-        .filter((val) => {
-          return val % 2 === 0;
-        })
-        .map((value) => (
-          <div>Row {value}</div>
-        ))}
+      <div>
+        <h1>Donation History</h1>
+      </div>
+      <div>
+        <div>
+          <h2>ID</h2>
+          <h3>Blood Type(s)</h3>
+          <h3>Type</h3>
+          <h3>Date deleted</h3>
+          <h3>Response count</h3>
+        </div>
+      </div>
     </div>
   );
 }
